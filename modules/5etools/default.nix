784c822354674e41f336028d5f064606a466837a
Adam Swinford <jaswinford@intxlog.com>
2024-10-23 09:06:56 -0400

    Revert "fix(5etools): added missing variable"
    This reverts commit bd98b715b4a0c28a6df39d22e78d3e0b29805aaf.

--- modules/5etools/default.nix
+++ modules/5etools/default.nix
@@ -7,12 +7,16 @@
     80
   ];
 
+  "5etools-link" = {
+    link = repo-5etools;
+    target = "/var/www/5etools";
+  };
   services.nginx.enable = true;
   services.nginx.virtualHosts."5etools.jadam.space" = {
     root = "/var/www/5etools";
   };
 
   services.cron.systemCronJobs = [
-    "0 * * * * cd /var/www/5etools && git pull"
+    "0 * * * * cd /var/www/5etools && git pull && nixos-rebuild switch"
   ];
 }
