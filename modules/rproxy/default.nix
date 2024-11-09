ba23a8a1afddff94d8c6cbaa4286b11b12c6fd49
Adam Swinford <talwyn.87@gmail.com>
2024-11-08 20:49:59 -0500

    feat(rproxy): added additional header configs to nextcloud

--- modules/rproxy/default.nix
+++ modules/rproxy/default.nix
@@ -74,6 +74,14 @@
      SSL
     // {
       locations."/".proxyPass = "http://10.0.1.20";
+        locations."/".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Protocol $scheme;
+          proxy_set_header X-Forwarded-Host $http_host;
+          proxy_buffering off;'';
     };
     "pve.jadam.space" =
       SSL
