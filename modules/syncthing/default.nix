75f52cc1c3354c38848827f3e2fb1ac074f28dd9
Adam Swinford <jaswinford@intxlog.com>
2024-11-27 12:28:57 -0500

    feat(syncthing): Added initial syncthing config
    Created key and certs for Freyr and IEL-100123
    Added encrypted certs via agenix
    Each machine config will need to define syncthing-key and syncthing-cert paths using agenix
    Syncthing main config will reference the agenix paths to populate key and cert
    folders and devices are centrally managed.

---
+++ modules/syncthing/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  services.syncthing = {
+    enable = true;
+    user = "adam";
+    openDefaultPorts = true;
+    settings = {
+      gui = {
+        theme = "black";
+      };
+      options = {
+        localAnnounceEnabled = false;
+      };
+
+      folders = {
+        "${config.services.syncthing.dataDir}/documents" = {
+          id = "documents";
+          devices = ["IEL-100123" "Freyr" "Adam-Cell"];
+        };
+      };
+      devices = {
+        IEL-100123 = {id = "BAU7YPT-DM4BGQ3-42XT25T-J67NVE6-UT3CEQS-ZSF6AJZ-ON2IJUE-WE6CMQA";};
+        Freyr = {id = "ZKQLFXV-TEIG4DH-JXE4SD3-OXQUMKO-HEQA6E4-JZ5IMMN-DWPYFZJ-RTH5QAN";};
+        Adam-Cell = { id = "67QZ2EE-7WP3ZTG-PIIK2JU-WVXDQQA-N5YXFOS-LIN6NGI-QVIULOP-OOBYUAR";};
+      };
+    };
+
+    overrideFolders = true;
+    overrideDevices = true;
+  };
+
+  services.syncthing = {
+    key = "${config.age.secrets.syncthing-key.path}";
+    cert = "${config.age.secrets.syncthing-cert.path}";
+  };
+
+  # Disable creation of default Sync folder.
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
+}
