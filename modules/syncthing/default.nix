99ac2eaa5c62eee20ec05eaa64dbb64575643241
Adam Swinford <talwyn.87@gmail.com>
2024-11-29 10:40:00 -0500

    feat(syncthing): Configured Sync folder and enabled auto-accept folders

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -22,6 +22,10 @@
           id = "documents";
           devices = ["IEL-100123" "Freyr" "Adam-Cell" "legion"];
         };
+        "${config.services.syncthing.dataDir}/Sync" = {
+          id = "default";
+          devices = [ "IEL-100123" "Freyr" "Adam-Cell" "legion"];
+        };
       };
       devices = {
         legion = {
@@ -29,24 +33,28 @@
           addresses = [
             "tcp://100.99.117.10:22000"
           ];
+          autoAcceptFolders = true;
         };
         IEL-100123 = {
           id = "BAU7YPT-DM4BGQ3-42XT25T-J67NVE6-UT3CEQS-ZSF6AJZ-ON2IJUE-WE6CMQA";
           addresses = [
             "tcp://100.69.44.30:22000"
           ];
+          autoAcceptFolders = true;
         };
         Freyr = {
           id = "ZKQLFXV-TEIG4DH-JXE4SD3-OXQUMKO-HEQA6E4-JZ5IMMN-DWPYFZJ-RTH5QAN";
           addresses = [
             "tcp://100.109.149.117:22000"
           ];
+          autoAcceptFolders = true;
         };
         Adam-Cell = { 
           id = "67QZ2EE-7WP3ZTG-PIIK2JU-WVXDQQA-N5YXFOS-LIN6NGI-QVIULOP-OOBYUAR";
           addresses = [
             "tcp://100.90.179.11:22000"
           ];
+          autoAcceptFolders = true;
         };
       };
     };
