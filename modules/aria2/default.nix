05a257d9334ac0e35cb34011f24f1d26cb15616b
Adam Swinford <talwyn.87@gmail.com>
2024-12-01 00:08:59 -0500

    feat(aria2): Generated RPC Toekn
    Created an RPC token and encoded it using agenix.

--- modules/aria2/default.nix
+++ modules/aria2/default.nix
@@ -5,6 +5,10 @@
   ...
 }: {
 
+age.secrets.rpc-token = {
+  file = ../../secrets/aria2-rpc-token.age;
+};
+
 networking.firewall.allowedTCPPorts = [
   80
 ];
@@ -24,6 +28,7 @@ services.aria2 = {
     enable-rpc = true;
     dir = "/var/lib/aria2/Downloads";
   };
+  rpcSecretFile = config.age.secrets.rpc-token.path;
   openPorts = true;
 };
 
