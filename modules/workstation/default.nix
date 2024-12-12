e9237e635124c17490973329475d8c1bdf6b352f
Adam Swinford <jaswinford@intxlog.com>
2024-12-12 09:29:48 -0500

    feat(vscode): Added vscode to home-manager config for workstations

--- modules/workstation/default.nix
+++ modules/workstation/default.nix
@@ -7,10 +7,26 @@
 
 environment.systemPackages = with pkgs; [
   obsidian
-  logseq
-  vscode-fhs
 ];
 
+home-manager.users.adam = {
+  programs.vscode = {
+    enable = true;
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      redhat.vscode-yaml
+      davidanson.vscode-markdownlint
+      ms-kubernetes-tools.vscode-kubernetes-tools
+      tim-koehler.helm-intellisense
+      golang.go
+      ms-python.python
+      ms-python.pylint
+      sourcery.sourcery
+    ];
+  };
+};
 nixpkgs.config.permittedInsecurePackages = [
   "electron-27.3.11"
 ];
