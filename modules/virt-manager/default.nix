5eba5bf60ee5fe32534e58fa5c1e8aae6d5d9111
Adam Swinford <talwyn.87@gmail.com>
2024-12-28 16:16:20 -0500

    feat: First draft of Virt-Manager module

---
+++ modules/virt-manager/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  programs.dconf.enable = true;
+
+  users.users.adam.extraGroups = [ "libvirtd" ];
+
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
+
+  virtualization = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+}
