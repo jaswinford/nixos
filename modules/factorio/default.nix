5bb8d62d1fe7b482b65b73c5e7a8d68e1abd9073
Adam Swinford <talwyn.87@gmail.com>
2024-12-29 12:55:04 -0500

    feat: First draft of Factorio module

---
+++ modules/factorio/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+
+  services.factorio = {
+    enable = true;
+    openFirewall = true;
+
+    # Factorio.com login
+    username = "talwyn";
+    token = "0813c19fe29f1696a0411c5e9595ff";
+    
+    # Save management
+    autosave-interval = 5;
+    nonBlockingSaving = true;
+    loadLatestSave = true;
+    saveName = "grotto";
+
+    # Game listing
+    public = true;
+    game-password = "bathysphere";
+    game-name = "Gamers Grotto";
+    description = "Private server for the members of the Gamers Grotto";
+
+    # Player Management
+    admins = [
+      "talwyn"
+    ];
+  };
+}
